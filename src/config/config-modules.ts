b3a02b0d74c0ab03e8e1d215c276f1c6d5e7240f
RavenPoe1900 <ravenpoe1900@gmail.com>
2025-08-10 12:13:32 -0400

    feat(notifications): Detail implementation of email template and batching

--- src/config/config-modules.ts
+++ src/config/config-modules.ts
@@ -1,5 +1,6 @@
 import { UsersModule } from 'src/modules/users/users.module';
 import { UserRoleModule } from 'src/modules/user-roles/user-roles.module';
 import { AuthModule } from 'src/auth/auth.module';
+import { NotificationsModule } from 'src/modules/notifications/notifications.module';
 
-export const MODULES = [UsersModule, UserRoleModule, AuthModule];
+export const MODULES = [UsersModule, UserRoleModule, AuthModule, NotificationsModule];
